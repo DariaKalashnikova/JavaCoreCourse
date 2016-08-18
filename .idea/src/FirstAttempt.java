fbd44e85d9ec18ee5881d7260a52584b19444aeb
Daria <cat_dasha@mail.ru>
2016-08-19 00:22:15 +0300

    first attempt

---
+++ .idea/src/FirstAttempt.java
@@ -0,0 +1,8 @@
+/**
+ * Created by User on 19.08.2016.
+ */
+public class FirstAttempt {
+    public static void main(String[] args) {
+        System.out.println("Panda saw a little monkey");
+    }
+}
