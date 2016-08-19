483e7767582f2b4dc42bc0832590c603e3ed72be
DariaKalashnikova <cat_dasha@mail.ru>
2016-08-19 12:37:09 +0300

    Commented a line

---
+++ .idea/src/TestName.java
@@ -0,0 +1,10 @@
+/**
+ * Created by office on 18.08.2016.
+ */
+public class TestName {
+    public static void main(String[] args) {
+        System.out.println("Test print");
+        System.out.print("One string");
+        //print and println shown in two lines
+    }
+}
